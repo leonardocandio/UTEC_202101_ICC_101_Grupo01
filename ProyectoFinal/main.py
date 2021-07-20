06bc800e1341a7c48d2189695314953bed1253c3
Leonardo Candio <leonardo.candio@utec.edu.pe>
2021-07-19 22:05:37 -0500

    Proyecto Final v0.1.1

--- ProyectoFinal/main.py
+++ ProyectoFinal/main.py
@@ -1,20 +1,33 @@
+# sourcery skip: ensure-file-closed
 import pygame as pg
 from pygame.locals import *
+import json
 
 pg.init()
 
 gametitle = ""
-screen_width = 1000
-screen_heigth = 1000
+screen_width = 900
+screen_heigth = 900
 
 screen = pg.display.set_mode((screen_width, screen_heigth))
 pg.display.set_caption(gametitle)
 
 gameRun = True
 
+scores_json = open("scores.json", "a+")
+
+try:
+    scores = json.load(scores_json)
+except json.decoder.JSONDecodeError:
+    pass
+
+
 while gameRun:
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            run = FALSE
+            gameRun = False
 
 pg.quit()
+
+
+scores_json.close()
